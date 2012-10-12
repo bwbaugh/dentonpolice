b29ecb745268e88558b39688e7308a5cf26ecf48
B. W. Baugh <brian@brianbaugh.com>
2012-10-11 22:58:23 -0500

    Refactor: make whitespace more consistent
    Changed from using 3 spaces to 4 spaces for indentation.

--- gmail_dentonpolice.py
+++ gmail_dentonpolice.py
@@ -10,8 +10,8 @@
 """Sends an email with an attachment via SMTP using gmail servers.
 
 Configuration:
-   GMAIL_USER: String of the gmail acount username to use.
-   GMAIL_PWD: String of the gmail acount password to use.
+    GMAIL_USER: String of the gmail acount username to use.
+    GMAIL_PWD: String of the gmail acount password to use.
 """
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -23,37 +23,37 @@ import os
 
 # Configuration
 
-GMAIL_USER = ''
+GMAIL_USER = ""
 GMAIL_PWD = ""
 if not GMAIL_USER or not GMAIL_PWD:
-   raise ImportError
+    raise ImportError
 
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 
 
 def mail(to, subject, text, attach):
-   """Send an email with attachment."""
-   msg = MIMEMultipart()
+    """Send an email with attachment."""
+    msg = MIMEMultipart()
 
-   msg['From'] = GMAIL_USER
-   msg['To'] = to
-   msg['Subject'] = subject
+    msg['From'] = GMAIL_USER
+    msg['To'] = to
+    msg['Subject'] = subject
 
-   msg.attach(MIMEText(text))
+    msg.attach(MIMEText(text))
 
-   part = MIMEBase('application', 'octet-stream')
-   part.set_payload(open(attach, 'rb').read())
-   encoders.encode_base64(part)
-   part.add_header('Content-Disposition',
-           'attachment; filename="%s"' % os.path.basename(attach))
-   msg.attach(part)
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(attach, 'rb').read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
+                    'attachment; filename="%s"' % os.path.basename(attach))
+    msg.attach(part)
 
-   mail_server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-   mail_server.ehlo()
-   mail_server.starttls()
-   mail_server.ehlo()
-   mail_server.login(GMAIL_USER, GMAIL_PWD)
-   mail_server.sendmail(GMAIL_USER, to, msg.as_string())
-   # Should be mail_server.quit(), but that crashes...
-   mail_server.close()
+    mail_server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    mail_server.ehlo()
+    mail_server.starttls()
+    mail_server.ehlo()
+    mail_server.login(GMAIL_USER, GMAIL_PWD)
+    mail_server.sendmail(GMAIL_USER, to, msg.as_string())
+    # Should be mail_server.quit(), but that crashes...
+    mail_server.close()
