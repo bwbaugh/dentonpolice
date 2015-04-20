49e8eff34116be6756d5d0155dbc6756c4496e46
Wesley Baugh <wesley@bwbaugh.com>
2015-04-19 22:58:51 -0700

    Set up basic automated test infrastructure
    Real tests will come later. This just lays the groundwork.
    
    References GH-9.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import setuptools
+
+
+setuptools.setup(
+    name='dentonpolice',
+    author='Wesley Baugh',
+    author_email='wesley@bwbaugh.com',
+    url='https://github.com/bwbaugh/dentonpolice',
+    license=(
+        'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 '
+        'Unported License'
+    ),
+    packages=setuptools.find_packages(exclude=['tests']),
+    setup_requires=['setuptools'],
+    # These are the packages that are explicitly depend on.
+    # On the other hand, the packages in `requirements.txt` include
+    # all implicit dependencies i.e., the packages required by the
+    # packages that the service depends on.
+    install_requires=[
+        'PyYAML>=3.11',
+        'twython>=3.1.2',
+    ],
+)
