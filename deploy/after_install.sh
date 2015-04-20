6aa820bc19e9fcbb25ddb4827ac429d951613919
Wesley Baugh <wesley@bwbaugh.com>
2015-04-19 22:48:50 -0700

    Fix deployment working directory with CodeDeploy

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -5,13 +5,19 @@ set -o pipefail
 
 install_dir="/srv/dentonpolice"
 
+# Using `pushd` and `popd` to play nice with the CloudDeploy agent.
+pushd ${install_dir}
+
 # Remove the old virtualenv, if it exists. This could potentially be
 #   put in `application_stop.sh`, however it might be useful to keep
 #   the virtualenv around for manual debugging.
-rm -rf ${install_dir}/venv/
+rm -rf venv/
 
-virtualenv --no-site-packages --python python3 ${install_dir}/venv
-${install_dir}/venv/bin/pip install -r ${install_dir}/requirements.txt
+virtualenv --no-site-packages --python python3 venv
+venv/bin/pip install -r requirements.txt
 
 # TODO(bwbaugh|2015-04-19): Create this if needed in the app.
-mkdir -p ${install_dir}/mugs
+mkdir -p mugs
+
+# Using `pushd` and `popd` to play nice with the CloudDeploy agent.
+popd
--- deploy/application_start.sh
+++ deploy/application_start.sh
@@ -6,9 +6,15 @@ set -o pipefail
 install_dir="/srv/dentonpolice"
 log_location="/var/log/dentonpolice"
 
+# Using `pushd` and `popd` to play nice with the CloudDeploy agent.
+pushd ${install_dir}
+
 # TODO(bwbaugh|2015-04-19): Configure proper logging.
 mkdir -p ${log_location}
-PYTHONPATH="${install_dir}" ${install_dir}/venv/bin/python -m dentonpolice \
+${install_dir}/venv/bin/python -m dentonpolice \
   >> ${log_location}/dentonpolice_stdout.log \
   2>> ${log_location}/dentonpolice_stderr.log \
   < /dev/null &
+
+# Using `pushd` and `popd` to play nice with the CloudDeploy agent.
+popd
