6e3f25cdf2e9c0ffa98278badb66b8eb7065d280
Wesley Baugh <wesley@bwbaugh.com>
2015-05-03 21:51:51 -0700

    Split crawler into smaller helper functions
    Needs more work, but it's a start at refactoring.

--- dentonpolice/crawler.py
+++ dentonpolice/crawler.py
@@ -26,6 +26,43 @@ from dentonpolice import twitter
 log = logging.getLogger(__name__)
 
 
+def main(bucket):
+    """Main function
+
+    Performs the following steps:
+
+    1.  Scrape the Jail Custody Report.
+    2.  Process to find new inmates that need to be posted.
+    3.  Download mug shots.
+    4.  Save a log.
+    5.  Upload to Twitter.
+    """
+    if _should_throttle(at_time=time.time()):
+        return
+    html = _get_jail_report(bucket=bucket)
+    # Parse list of inmates from webpage
+    inmates = jail.parse_inmates(html)
+    # Get mug shots for every current inmate. (GH-12)
+    try:
+        jail.get_mug_shots(inmates=inmates, bucket=bucket)
+    except (http.client.HTTPException, urllib.error.URLError) as error:
+        log.warning('Other error while getting mug shots: %r', error)
+        return
+    storage.save_mug_shots(inmates)
+    # Make a copy of the current parsed inmates to use later
+    inmates_original = inmates[:]
+    inmates = inmate_module.extract_inmates_to_process(
+        inmates=inmates,
+        recent_inmates=[
+            inmate_module.Inmate.from_dict(data)
+            for data in storage.read_log(recent=True)
+        ],
+    )
+    _publish_new_inmates(inmates=inmates, inmates_original=inmates_original)
+    _publish_record_count(inmates=inmates_original)
+    _publish_updated_inmates(inmates=inmates_original)
+
+
 def _should_throttle(at_time):
     minimum_report_time = at_time - staticconf.read('minimum_report_age_s')
     try:
@@ -48,19 +85,7 @@ def _should_throttle(at_time):
     return False
 
 
-def main(bucket):
-    """Main function
-
-    Performs the following steps:
-
-    1.  Scrape the Jail Custody Report.
-    2.  Process to find new inmates that need to be posted.
-    3.  Download mug shots.
-    4.  Save a log.
-    5.  Upload to Twitter.
-    """
-    if _should_throttle(at_time=time.time()):
-        return
+def _get_jail_report(bucket):
     html = jail.get_jail_report()
     if html is None:
         # Without a report, there is nothing to do.
@@ -80,102 +105,104 @@ def main(bucket):
             html=html,
             timestamp=datetime.datetime.utcnow(),
         )
-    # Parse list of inmates from webpage
-    inmates = jail.parse_inmates(html)
-    # Get mug shots for every current inmate. (GH-12)
-    try:
-        jail.get_mug_shots(inmates=inmates, bucket=bucket)
-    except (http.client.HTTPException, urllib.error.URLError) as error:
-        log.warning('Other error while getting mug shots: %r', error)
-        return None
-    storage.save_mug_shots(inmates)
-    # Make a copy of the current parsed inmates to use later
-    inmates_original = inmates[:]
-    inmates = inmate_module.extract_inmates_to_process(
-        inmates=inmates,
-        recent_inmates=[
-            inmate_module.Inmate.from_dict(data)
-            for data in storage.read_log(recent=True)
-        ],
+    return html
+
+
+def _publish_new_inmates(inmates, inmates_original):
+    """Log and post to Twitter."""
+    # Discard inmates that we couldn't save a mug shot for.
+    inmates = [inmate for inmate in inmates if inmate.mug]
+    if not inmates:
+        return
+    sorted_by_arrest = sorted(
+        inmates,
+        key=inmate_module.Inmate.sort_key_for_arrest,
     )
-    # We now have our final list of inmates, so let's process them.
-    if inmates:
-        # Discard inmates that we couldn't save a mug shot for.
-        inmates = [inmate for inmate in inmates if inmate.mug]
-        # Log and post to Twitter.
+    twitter_client = twitter.get_twitter_client()
+    if twitter_client is not None:
         try:
-            twitter_client = twitter.get_twitter_client()
-            if twitter_client is not None:
-                sorted_by_arrest = sorted(
-                    inmates,
-                    key=inmate_module.Inmate.sort_key_for_arrest,
+            for inmate in sorted_by_arrest:
+                mug_shot_fname = 'mugs/{}'.format(
+                    storage.most_recent_mug(inmate),
                 )
-                for inmate in sorted_by_arrest:
-                    mug_shot_fname = 'mugs/{}'.format(
-                        storage.most_recent_mug(inmate),
+                log.debug('Media fname: %s', mug_shot_fname)
+                with open(mug_shot_fname, mode='rb') as mug_shot_file:
+                    twitter.tweet_mug_shots(
+                        twitter_client=twitter_client,
+                        inmate=inmate,
+                        caption=twitter.get_twitter_message(inmate),
+                        mug_shot_file=mug_shot_file,
                     )
-                    log.debug('Media fname: %s', mug_shot_fname)
-                    with open(mug_shot_fname, mode='rb') as mug_shot_file:
-                        twitter.tweet_mug_shots(
-                            twitter_client=twitter_client,
-                            inmate=inmate,
-                            caption=twitter.get_twitter_message(inmate),
-                            mug_shot_file=mug_shot_file,
-                        )
         finally:
             # Still want to log even if there was an uncaught error
             # while posting to Twitter.
             storage.log_inmates(inmates)
-        # Remove any inmates that failed to post so they're retried.
-        posted = inmates_original[:]
-        for inmate in inmates:
-            if not inmate.posted:
-                posted = [x for x in posted if x.id != inmate.id]
-        # Save the most recent list of inmates to the log for next time
-        storage.log_inmates(posted, recent=True)
+    # Remove any inmates that failed to post so they're retried.
+    posted = inmates_original[:]
+    for inmate in inmates:
+        if not inmate.posted:
+            posted = [x for x in posted if x.id != inmate.id]
+    # Save the most recent list of inmates to the log for next time
+    storage.log_inmates(posted, recent=True)
+
+
+def _publish_record_count(inmates):
     # Check if there is a new record number of inmates seen on the jail report.
     (most_count, on_date) = storage.get_most_inmates_count()
-    count = len(inmates_original)
-    if not most_count or count > most_count:
-        twitter_client = twitter.get_twitter_client()
-        if twitter_client is not None:
-            twitter.tweet_most_count(
-                twitter_client=twitter_client,
-                count=count,
-                most_count=most_count,
-                on_date=on_date,
-            )
-            # Only log if we published the record.
-            storage.log_most_inmates_count(count)
+    count = len(inmates)
+    log.debug(
+        'Current count is %d. Most count was %d on %s',
+        count,
+        most_count,
+        on_date,
+    )
+    if most_count and count <= most_count:
+        return
+    twitter_client = twitter.get_twitter_client()
+    if twitter_client is None:
+        return
+    twitter.tweet_most_count(
+        twitter_client=twitter_client,
+        count=count,
+        most_count=most_count,
+        on_date=on_date,
+    )
+    # Only log if we published the record.
+    storage.log_most_inmates_count(count)
+
+
+def _publish_updated_inmates(inmates):
     updated_records = inmate_module.extract_updated_inmates(
         inmates=[
             inmate
-            for inmate in inmates_original
+            for inmate in inmates
             # Only consider inmates that don't look new, and only those
             #   that have a mug shot.
             if inmate not in inmates and inmate.mug
         ],
     )
-    if updated_records:
-        try:
-            twitter_client = twitter.get_twitter_client()
-            if twitter_client is not None:
-                for record in updated_records:
-                    twitter.tweet_mug_shots(
-                        twitter_client=twitter_client,
-                        inmate=record['inmate'],
-                        caption=(
-                            'Found a newer mug shot for {name}. ({arrest})'
-                        ).format(
-                            name=record['inmate'].first_name,
-                            arrest=record['inmate'].arrest,
-                        ),
-                        mug_shot_file=io.BytesIO(record['inmate'].mug),
-                        in_reply_to_status_id=record['last_tweet_id'],
-                    )
-        finally:
-            # Still want to log even if there was an uncaught error
-            # while posting to Twitter.
-            storage.log_inmates(
-                inmates=[record['inmate'] for record in updated_records],
+    if not updated_records:
+        return
+    twitter_client = twitter.get_twitter_client()
+    if twitter_client is None:
+        return
+    try:
+        for record in updated_records:
+            twitter.tweet_mug_shots(
+                twitter_client=twitter_client,
+                inmate=record['inmate'],
+                caption=(
+                    'Found a newer mug shot for {name}. ({arrest})'
+                ).format(
+                    name=record['inmate'].first_name,
+                    arrest=record['inmate'].arrest,
+                ),
+                mug_shot_file=io.BytesIO(record['inmate'].mug),
+                in_reply_to_status_id=record['last_tweet_id'],
             )
+    finally:
+        # Still want to log even if there was an uncaught error
+        # while posting to Twitter.
+        storage.log_inmates(
+            inmates=[record['inmate'] for record in updated_records],
+        )
